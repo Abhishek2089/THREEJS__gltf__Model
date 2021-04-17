5c964162ea06f0c0cf4f724c4085d8191d3984df
Grandfather208 <72085850+Grandfather208@users.noreply.github.com>
2021-04-17 21:47:02 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,55 @@
+let scene, camera, renderer;
+    function init() {
+        scene = new THREE.Scene();
+        scene.background = new THREE.Color(0xdddddd);
+        camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,1,5000);
+        camera.rotation.y = 45/180*Math.PI;
+        camera.position.x = 1200;
+        camera.position.y = 500;
+        camera.position.z = 1000;
+
+        renderer = new THREE.WebGLRenderer({antialias:true});
+        renderer.setSize(window.innerWidth,window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+
+
+        controls = new THREE.OrbitControls(camera,renderer.domElement);
+        //controls.addEventListener('change', renderer);
+		controls.update();
+        hlight = new THREE.AmbientLight (0x404040,100);
+        scene.add(hlight);
+        directionalLight = new THREE.DirectionalLight(0xffffff,100);
+        directionalLight.position.set(0,1,0);
+        directionalLight.castShadow = true;
+        scene.add(directionalLight);
+        light = new THREE.PointLight(0xc4c4c4,10);
+        light.position.set(0,300,500);
+        scene.add(light);
+        light2 = new THREE.PointLight(0xc4c4c4,10);
+        light2.position.set(500,100,0);
+        scene.add(light2);
+        light3 = new THREE.PointLight(0xc4c4c4,10);
+        light3.position.set(0,100,-500);
+        scene.add(light3);
+        light4 = new THREE.PointLight(0xc4c4c4,10);
+        light4.position.set(-500,300,500);
+        scene.add(light4);
+
+
+
+        let loader = new THREE.GLTFLoader();
+        loader.load('scene.gltf', function(gltf){
+            car = gltf.scene.children[0];
+            car.scale.set(50,50,50);
+            scene.add(gltf.scene);
+			console.log("successfull")
+            animate();
+        }, undefined, function (error) {
+			console.log(error);
+		});
+    }
+    function animate() {
+        renderer.render(scene,camera);
+        requestAnimationFrame(animate);
+    }
+    init();
